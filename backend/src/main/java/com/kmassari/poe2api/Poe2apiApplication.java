9c664eb48f014e0104d138e42f0444e8ec52c0d1
Kenny Massari <kenny.massari315@gmail.com>
2026-03-30 15:06:28 -0500

    Step 1: Setup Environment - Compile: SUCCESS | Tests: N/A

---
+++ backend/src/main/java/com/kmassari/poe2api/Poe2apiApplication.java
@@ -0,0 +1,11 @@
+package com.kmassari.poe2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poe2apiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Poe2apiApplication.class, args);
+    }
+}
