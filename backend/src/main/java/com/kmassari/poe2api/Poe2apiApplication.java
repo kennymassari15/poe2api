9f6cb5feaaabe7d065c237c230fb0feed82f251c
Kenny Massari <kenny.massari315@gmail.com>
2026-03-31 08:28:35 -0500

    completed backend which will grab open port instead of 8080

--- backend/src/main/java/com/kmassari/poe2api/Poe2apiApplication.java
+++ backend/src/main/java/com/kmassari/poe2api/Poe2apiApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Poe2apiApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(Poe2apiApplication.class, args);
-    }
+public class Poe2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poe2ApiApplication.class, args);
+	}
+
 }
