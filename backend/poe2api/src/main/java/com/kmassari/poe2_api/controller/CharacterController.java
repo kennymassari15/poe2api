569201fb626db4df2e1d772790abac5fcde1ed45
Kenny Massari <kenny.massari315@gmail.com>
2026-04-01 09:48:40 -0500

    fixed swagger page

--- backend/poe2api/src/main/java/com/kmassari/poe2_api/controller/CharacterController.java
+++ backend/poe2api/src/main/java/com/kmassari/poe2_api/controller/CharacterController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,13 +54,13 @@ public class CharacterController {
             .orElse(ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        if (!repo.existsById(id)) {
-            return ResponseEntity.notFound().build();
-        }
+    // @DeleteMapping("/{id}")
+    // public ResponseEntity<Void> delete(@PathVariable Long id) {
+    //     if (!repo.existsById(id)) {
+    //         return ResponseEntity.notFound().build();
+    //     }
 
-        repo.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
+    //     repo.deleteById(id);
+    //     return ResponseEntity.noContent().build();
+    // }
 }
